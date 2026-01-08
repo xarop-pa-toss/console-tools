0d3e2cd85adf32a5405fc62ade1ecbfb2e6e91d3
ricardosantos-infodinamica <ricardosantos@infodinamica.pt>
2026-01-08 10:52:03 +0000

    feat: add AppPaths global vars.

---
+++ src/MotorDArranque/AppPaths.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MotorDArranque
+{
+    internal static class AppPaths
+    {
+        public static readonly string UserTemp = Path.Combine(Path.GetTempPath(), "MotorDArranque");
+        public static readonly string InstallRoot = String.Empty;
+    }
+}
